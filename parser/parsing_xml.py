b2a29b1539167b1fbe33e29ff9ca628b02dec021
vavasik800 <74983123+vavasik800@users.noreply.github.com>
2023-01-23 02:03:46 +0300

    Add method json_to_dict for convert  json to a one-dimensional dictionary.

--- parser/parsing_xml.py
+++ parser/parsing_xml.py
@@ -3,6 +3,12 @@ from xml.etree.ElementTree import Element
 from typing import List
 
 
+def __con_dicts(dict1: dict, dict2: dict) -> dict:
+    """
+    Конкантинация словарей
+    """
+    return dict(list(dict1.items()) + list(dict2.items()))
+
 def __tag_to_json(tag: Element):
     """
     Метод для получения всех значимых атрибутов листа.
@@ -26,11 +32,11 @@ def __parsing_entry_xml(xml_entry: Element) -> dict:
     response[tag_name] = entry
     if len(xml_entry) == 0:
         # вызов для самого последнего листа дерева
-        response[tag_name] = dict(list(response[tag_name].items()) + list({'text': xml_entry.text or ''}.items()))
+        response[tag_name] = __con_dicts(response[tag_name], {'text': xml_entry.text or ''})
     else:
         for child in xml_entry:
             # рекурсивный вызов и дальнейший обход
-            response[tag_name] = dict(list(response[tag_name].items()) + list(__parsing_entry_xml(child).items()))
+            response[tag_name] = __con_dicts(response[tag_name], __parsing_entry_xml(child))
     return response
 
 
@@ -46,5 +52,22 @@ def parsing_xml(path_file_xml: str) -> List[dict]:
     for elem in root:
         result.append(__parsing_entry_xml(elem))
     return result
+
+
+def json_to_dict(json, name_tag=None) -> dict:
+    """
+    Рекурсивный метод для преобразования json - формата в одномерный словарь.
+    :param json: json - формат
+    :param name_tag: - имя тэга выше (для соединения полей)
+    :return: одномерный словарь
+    """
+    response = {}
+    for key in json:
+        new_key = key if name_tag is None else f'{name_tag}_{key}'
+        if type(json[key]) == dict:
+            response = __con_dicts(response, json_to_dict(json[key], key))
+        else:
+            response[new_key] = json[key]
+    return response
 
 
