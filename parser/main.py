6c41bb8470aba414255ec778148fc3479e84c8d7
vavasik800 <74983123+vavasik800@users.noreply.github.com>
2023-01-25 02:09:39 +0300

    New main for test different run-functions

--- parser/main.py
+++ parser/main.py
@@ -1,11 +1,38 @@
+import glob
+import os
+import time
+
 from multi_readers import WorkWithXmlMultiProcess, write_txt
 
 
 def main():
-    path_file_xml = 'data/00000_2015_11_04_12_12_12_111.xml'
+    # path_file_xml = 'data/00000_2015_11_04_12_12_12_111.xml'
+    path_file_xml = 'data/test_big_xml.xml'
     path_out_dir = 'out/'
-    a = WorkWithXmlMultiProcess(path_file_xml, write_txt, path_out_dir, 3)
+    a = WorkWithXmlMultiProcess(path_file_xml, write_txt, path_out_dir, 1000)
+    print('Считываем весь файл')
+    start = time.time()
     a.run()
+    finish = time.time()
+    print("Время выполнения функции, сначала считав весь файл - ", start - finish)
+    # time.sleep(3)
+    # files = glob.glob(path_out_dir + '*')
+    # for f in files:
+    #     os.remove(f)
+    # time.sleep(3)
+    # # print('Считываем файл последовательно')
+    # # start = time.time()
+    # # a.run()
+    # # finish = time.time()
+    # # print("Время выполнения функции, читая файл последовательно - ", start - finish)
+    # # for f in files:
+    # #     os.remove(f)
+    # # time.sleep(3)
+    # print('Без потоков')
+    # start = time.time()
+    # a.run_2()
+    # finish = time.time()
+    # print("Время выполнения функции, без многопоточечности ", start - finish)
     return
 
 
